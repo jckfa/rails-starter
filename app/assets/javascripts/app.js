a6e6619b38f986d69b5a1088ca770bb4ae7ece01
Jack Halten Fahnestock <buttface@jackf.me>
2017-02-22 20:19:41 -0500

    rename application to app

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,12 @@
+/*
+
+Any JavaScript/Coffee file within
+- this directory,
+- lib/assets/javascripts,
+- vendor/assets/javascripts,
+- any plugin's vendor/assets/javascripts directory
+can be referenced here using a relative path.
+
+= require turbolinks
+
+*/
